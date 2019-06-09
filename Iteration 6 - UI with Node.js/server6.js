6ce60be8467d121f5379f989a1cb5e12c38b528a
Beth Ackerman [bea82] <bea82@arastudent.ac.nz>
2019-06-09 16:42:23 +1200

    Node files update to master files

---
+++ Iteration 6 - UI with Node.js/server6.js
@@ -0,0 +1,73 @@
+/* 
+Building a Node.js static file server (files over HTTP) using ES6+
+https://adrianmejia.com/blog/2016/08/24/building-a-node-js-static-file-server-files-over-http-using-es6/
+
+MIME Types List
+https://www.freeformatter.com/mime-types-list.html
+*/
+
+// can handle image file request as well
+
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+
+// maps file extention to MIME types
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword',
+  '.eot': 'appliaction/vnd.ms-fontobject',
+  '.ttf': 'aplication/font-sfnt'
+}
+
+// Create a server
+http.createServer(function (request, response) {
+  // Parse the request containing file name
+  let pathname = url.parse(request.url).pathname
+  // Get the extension of the required file
+  const ext = path.parse(pathname).ext
+
+  // Print the name of the file for which request is made.
+  console.log("Request for " + pathname + " received.")
+
+  // Read the requested file content from file system
+  var fileName = pathname.substr(1)
+  fs.readFile(fileName, function (err, data) {
+    if (err) {
+      console.log(err)
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      response.writeHead(404, {
+        'Content-Type': 'text/html'
+      })
+      response.end(`File ${pathname} not found!`)
+      return
+    }
+
+    // Corresponding file found	  
+    // HTTP Status: 200 : OK      
+    response.writeHead(200, {
+      'Content-Type': mimeType[ext]
+    })
+
+    // Write the content of the file to response body
+    response.write(data)
+
+    // Send the response body 
+    response.end()
+  })
+}).listen(8081)
+
+// Console will print the message
+console.log('Server running at localhost:8081/')
---
+++ Iteration 6 - UI with Node.js/vue-Calc.js
@@ -0,0 +1,52 @@
+var calc = new Vue ({
+  el: '#calculation-app',
+  components: {
+    'file-loader': fileLoader,
+    'output-component': outputComponent
+  },
+  data : {
+        correlation: 0,
+        regression:0,
+        correlationDetResult: 0,
+        betaOne: 0,
+        betaZero: 0,
+        arrayX: [],
+        arrayY: [],
+        linearRegResult: [],
+        correlationDetCalculated: false,
+        linearRegCalculated: false
+  },
+  methods : {
+    getData (array) {
+      if (!this.arrayX.length) {
+        this.arrayX.push(...array)
+      } else {
+        this.arrayY.push(...array)
+        this.addData(this.arrayX, this.arrayY)
+        this.handleData()
+      }
+    },
+    handleData() {
+      this.correlationDetResult = this.calcCorrelation(this.arrayX, this.arrayY)
+      this.correlationDetCalculated = true
+      this.linearRegResult = this.calcLinearRegression(this.arrayX, this.arrayY)
+      this.linearRegCalculated = true
+    },
+    addData (arrayX, arrayY) {
+      // add data to new class
+      this.correlation = new CorrelationDetermination(arrayX, arrayY)
+      this.regression = new LinearRegression(arrayX, arrayY)
+      console.log(this.regression)
+    },
+    calcCorrelation (arrayX, arrayY) {
+      let result = this.correlation.calcCorrelation()
+      return result
+    },
+    calcLinearRegression (arrayX, arrayY) {
+      let result = this.regression.calcRegression()
+      this.betaOne = this.regression.regression[0]
+      this.betaZero = this.regression.regression[1]
+      return result
+    }
+  }
+})
