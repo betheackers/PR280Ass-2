8a23a0486bc6eb4a218df22f4588e45fcdefe16b
Betheackers <betheackers@gmail.com>
2019-06-10 15:46:56 +1200

    Some layout changes

--- Final Assignment 2/loader.js
+++ Final Assignment 2/loader.js
@@ -5,58 +5,49 @@
 /* eslint no-undef: "off" */
 var fileLoader = {
   template: `
-  <div>
-    <div class="row">
-          <div class="col-sm">
-            <h2>Select your first text file</h2>
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-              </div>
-              <div class="custom-file">
-                <input type="file" @change="onFileChange" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-              </div>
+<div>
+  <div class="row">
+        <div class="col-sm">
+          <h2>Select your first text file</h2>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+            </div>
+            <div class="custom-file">
+              <input type="file" @change="onFileChange" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+              <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
             </div>
           </div>
-          <div v-if="firstFileLoaded === true" class="col-sm">
-            <h2>Select a second text file</h2>
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
-              </div>
-              <div class="custom-file">
-                <input type="file" @change="onFileChange" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
-                <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
-              </div>
-            </div>
         </div>
-    </div>
-    <br><br>
-    <table class="table table-striped table-dark table-sm">
-      <thead style="text-align:center;">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">X</th>
-          <th scope="col">Y</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="n in firstArray.length">
-          <td style="text-align:center;"></td>
-          <td style="text-align:center;">{{ firstArray[n-1] }}</td>
-          <td style="text-align:center;">{{ secondArray[n-1] }}</td>
-        </tr>
-      </tbody>
-      <!--      <tfoot>
-                <tr>
-                <th>Totals</th>
-                <td>var1</td>
-                <td>var2</td>
-                </tr>
-                </tfoot>    -->
-    </table>
-  </div>`,
+        <div v-if="firstFileLoaded === true" class="col-sm">
+          <h2>Select a second text file</h2>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+            </div>
+            <div class="custom-file">
+              <input type="file" @change="onFileChange" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+              <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+            </div>
+          </div>
+      </div>
+  </div>
+  <br><br>
+  <table class="table table-striped table-dark table-sm" v-show="secondArray.length">
+    <thead class="text-center">
+      <tr>
+        <th scope="col">X</th>
+        <th scope="col">Y</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="n in firstArray.length">
+        <td class="text-center"> {{ firstArray[n-1] }}</td>
+        <td class="text-center"> {{ secondArray[n-1] }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>`,
   data: function () {
     return {
       firstArray: [],
